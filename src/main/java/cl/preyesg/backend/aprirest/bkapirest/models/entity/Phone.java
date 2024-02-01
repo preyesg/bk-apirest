e711fd7926dbfd62aa50a0e4adbed862fa0660fc
preyesg <97925318+preyesg@users.noreply.github.com>
2024-02-01 10:06:07 -0300

    Update Phone.java

--- src/main/java/cl/preyesg/backend/aprirest/bkapirest/models/entity/Phone.java
+++ src/main/java/cl/preyesg/backend/aprirest/bkapirest/models/entity/Phone.java
@@ -17,9 +17,13 @@ public class Phone {
     private String citycode;
     private String countrycode;
 
+    /*
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_usuario")
-    private Usuario idUsuario;
+     */
+    @ManyToOne(cascade= { CascadeType.ALL})
+    @JoinColumns(value = { @JoinColumn(referencedColumnName = "id")}, foreignKey = @ForeignKey(name = "id_usuario"))
+    private Usuario usuario;
 
     public long getId() {
         return id;
